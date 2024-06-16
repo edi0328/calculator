2624e5c6c56eea45494d72074fa2f296e1c312ec
edi0328 <edisonthai0328@gmail.com>
2024-06-15 20:59:38 -0400

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(op, a, b) {
+    if (op == '+') return add(a, b);
+    if (op == '-') return substract(a, b);
+    if (op == '*') return multiply(a, b);
+    if (op == '/') return divide(a, b);
+}
+
+let a;
+let b;
+let op;
