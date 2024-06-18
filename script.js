d952445324db63479e1e91e6dbc78476faa2b03a
edi0328 <edisonthai0328@gmail.com>
2024-06-17 21:57:53 -0400

    Fix operation logic part 1

--- script.js
+++ script.js
@@ -21,35 +21,38 @@ function operate(op, a, b) {
     if (op == '/') return divide(a, b);
 }
 
+// Update display when number is pressed
 function changeDisplay(content) {
     const display = document.querySelector("#display");
-    if (display.textContent == '0') display.textContent = content;
+    if (display.textContent == '0' || b == 0) display.textContent = content;
     else display.textContent += content;
     return Number(display.textContent);
 }
 
 let a;
-let b;
+let b = display.textContent;
 let op;
 
 // When button is pressed, display changes and stores number
 const numButtons = document.querySelectorAll(".num");
 numButtons.forEach(button => {
     button.addEventListener("click", () => {
-        if (op === undefined) a = changeDisplay(button.textContent);
-        else b = changeDisplay(button.textContent);
+        b = changeDisplay(button.textContent);
+
     })
 });
 
-// Pressing an operation stores the operation
+// Pressing an operation stores the operation and assigns the displayed value to a
 const opButtons = document.querySelectorAll(".op");
 opButtons.forEach(button => {
-    button.addEventListener("click", () => 
-        op = button.textContent
-)});
+    button.addEventListener("click", () => {
+        op = button.textContent;
+        a = b;
+        b = 0;
+})});
 
 const equalButton = document.querySelector("#eq");
 equalButton.addEventListener("click", () => {
-    operate(op, a, b);
+    b = changeDisplay(operate(op, a, b));
 });
 
